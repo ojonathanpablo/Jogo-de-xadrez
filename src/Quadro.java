ea61fa1af76e72e3e2a7c6b46b1eb1452ee35a2a
ojonathanpablo <jonathanpablocp@gmail.com>
2022-10-28 15:04:48 -0300

    Adicionando Exeções na classe Quadro

--- src/Quadro.java
+++ src/Quadro.java
@@ -4,6 +4,10 @@ public class Quadro {
     private Peca[][] pecas;
 
     public Quadro(int linhas,int colunas){
+        if(linhas <1 || colunas < 1){
+            throw new QuadroException("Error: é necessario que tenha uma linha ou uma coluna: ");
+        }
+
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
@@ -12,28 +16,46 @@ public class Quadro {
     public int getLinhas(){
         return linhas;
     }
-    public void setLinhas(int linhas){
-        this.linhas = linhas;
-    }
 
     public int getColunas(){
         return colunas;
     }
-    public void setColunas(int colunas){
-        this.colunas = colunas;
-    }
-
+// METODOS
     public Peca peca(int linha, int coluna){
+        if (!existeposicao(linha,coluna)){
+            throw new QuadroException("Error: Posição invalida");
+        }
         return pecas[linha][coluna];
     }
 
     public Peca peca( Posicao posicao){
+        if (!existeposicao(posicao)){
+            throw new QuadroException("Error: Posição invalida");
+        }
         return pecas[posicao.getFileira()][posicao.getColuna()];
     }
 
     public void colocaPeca(Peca peca, Posicao posicao){
+        if (existeUmaPeca(posicao)){
+            throw new QuadroException("Error: Posição: "+posicao+" Já está sendo ultilizada");
+        }
         pecas[posicao.getFileira()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
+    }
+
+    private boolean existeposicao(int linha, int coluna){
+       return linha >= 0 && linha < linhas && colunas >= 0 && coluna < colunas;     
+    }
+
+    public boolean existeposicao(Posicao posicao){
+        return existeposicao(posicao.getFileira(),posicao.getColuna());
+    }
+
+    public boolean existeUmaPeca(Posicao posicao){
+        if (!existeposicao(posicao)){
+            throw new QuadroException("Error: Posição invalida");
+        }
+        return peca(posicao) != null;
     }
 
 }
---
+++ src/QuadroException.java
@@ -0,0 +1,7 @@
+public class QuadroException extends RuntimeException{
+    private static final long serialVersionUID = 1L; 
+        
+    public QuadroException(String msg){
+        super(msg);
+    }
+}
