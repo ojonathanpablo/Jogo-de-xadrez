ba8ea31b47e802ae6ab0dcc041e2966b8531adbe
ojonathanpablo <jonathanpablocp@gmail.com>
2022-10-29 11:26:42 -0300

    Adicionando cores no terminal

--- src/PartidaDeXadrez.java
+++ src/PartidaDeXadrez.java
@@ -20,8 +20,18 @@ public class PartidaDeXadrez {
         quadro.colocaPeca(peca, new XadezPosicao(coluna, linha).toPosicao());
     }   
     private void configuracaoInicial(){
-        colocaNovaPeca('b', 6, new Torre(quadro, Cor.BRANCO));
-        colocaNovaPeca('e', 8, new Rei(quadro, Cor.PRETO));
-        colocaNovaPeca('e', 1, new Rei(quadro, Cor.BRANCO));
+        colocaNovaPeca('c', 1, new Torre(quadro, Cor.BRANCO));
+        colocaNovaPeca('c', 2, new Torre(quadro, Cor.BRANCO));
+        colocaNovaPeca('d', 2, new Torre(quadro, Cor.BRANCO));
+        colocaNovaPeca('e', 2, new Torre(quadro, Cor.BRANCO));
+        colocaNovaPeca('e', 1, new Torre(quadro, Cor.BRANCO));
+        colocaNovaPeca('d', 1, new Rei(quadro, Cor.BRANCO));
+
+        colocaNovaPeca('c', 7, new Torre(quadro, Cor.PRETO));
+        colocaNovaPeca('c', 8, new Torre(quadro, Cor.PRETO));
+        colocaNovaPeca('d', 7, new Torre(quadro, Cor.PRETO));
+        colocaNovaPeca('e', 7, new Torre(quadro, Cor.PRETO));
+        colocaNovaPeca('e', 8, new Torre(quadro, Cor.PRETO));
+        colocaNovaPeca('d', 8, new Rei(quadro, Cor.PRETO));
     }
 }
